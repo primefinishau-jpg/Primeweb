f61ad38493eb5881a2a5f6297a36abe0675ffc47
primefinishau-jpg <primefinish.au@gmail.com>
2026-07-21 01:43:03 +0530

    new changes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -101,6 +101,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico",
+      },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
